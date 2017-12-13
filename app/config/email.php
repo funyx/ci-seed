77b80bd65daadce99de36eb9e55c22225a9af02b
Dimitar Dobchev <ddobchev@gmail.com>
2017-12-13 14:30:15 +0200

    initial commit

---
+++ app/config/email.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+  $config['protocol'] = 'smtp';
+  $config['smtp_host'] = 'smtp.sendgrid.net';
+  $config['smtp_user'] = 'sendgridusername';
+  $config['smtp_pass'] = 'sendgridpassword';
+  $config['smtp_port'] = '587';
+  $config['crlf'] = '\r\n';
+  $config['newline'] = '\r\n';
+  $config['charset'] = 'iso-8859-1';
+  $config['wordwrap'] = TRUE;
